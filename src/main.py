3a83cfc7bbd5756436d0863c2f4465ee8e85d0d3
Aleksei Stepanishchev <steinach@mail.ru>
2026-07-29 20:11:21 +0200

    feat(app): add main.py

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+
+from src.container import Container
+from src.infrastructure.config.settings import settings
+from src.infrastructure.database.db import async_session_factory
+from src.infrastructure.telegram.bot import create_bot, create_dispatcher
+
+
+async def main() -> None:
+    logging.basicConfig(level=settings.log_level)
+
+    container = Container(session_factory=async_session_factory)
+    bot = create_bot(settings)
+    dp = create_dispatcher(container)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
